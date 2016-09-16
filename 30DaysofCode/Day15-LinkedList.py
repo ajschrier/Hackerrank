cc0ca0248f97d2dd83a6400290aa9dffb2aa9186
AJ Schrier <aschrier@apple.com>
2016-09-15 18:30:59 -0700

    Added Day15-LinkedList

---
+++ 30DaysofCode/Day15-LinkedList.py
@@ -0,0 +1,26 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Solution:
+    def display(self, head):
+        current = head
+        while current:
+            print current.data,
+            current = current.next
+# Beginning of exercise
+
+    def insert(self, head, data):
+        pass
+
+# End of exercise
+
+mylist = Solution()
+T = int(input())
+head = None
+for i in range(T):
+    data = int(input())
+    head = mylist.insert(head, data)
+mylist.display(head)
