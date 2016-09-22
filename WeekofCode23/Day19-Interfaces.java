44fc5738e815856c43d10e7abbd3e70888e48862
AJ Schrier <aschrier@apple.com>
2016-09-22 11:24:04 -0700

    Added Day19-Interfaces

---
+++ WeekofCode23/Day19-Interfaces.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+interface AdvancedArithmetic{
+   int divisorSum(int n);
+}
+// Create a class that adheres to AdvancedArithmetic here
+
+// end exercise
+class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.close();
+        
+        AdvancedArithmetic myCalculator = new Calculator(); 
+        int sum = myCalculator.divisorSum(n);
+        System.out.println("I implemented: " + myCalculator.getClass().getInterfaces()[0].getName() );
+        System.out.println(sum);
+    }
+}
