c559efbc75ecfcabd14b2746a3fd7f923eb35b41
AJ Schrier <ajschrier@gmail.com>
2016-12-17 16:59:40 -0800

    Completed DesignerPDFViewer

--- Python/DesignerPDFViewer.py
+++ Python/DesignerPDFViewer.py
@@ -1,10 +1,19 @@
 #!/bin/python
+import string
 
 
 def pdfSelection(h, word):
-    return word
+    heights = {}
+    for i in xrange(26):
+        heights[string.lowercase[i]] = h[i]
+    width = 0
+    for letter in word:
+        if int(heights[letter]) > width:
+            width = int(heights[letter])
+    return width * len(word)
 
 
 if __name__ == '__main__':
     h = map(int, raw_input().strip().split(' '))
     word = raw_input().strip()
+    print pdfSelection(h, word)
--- Python/DesignerPDFViewer_tests.py
+++ Python/DesignerPDFViewer_tests.py
@@ -5,8 +5,9 @@ import DesignerPDFViewer
 class pdfSelectionTests(unittest.TestCase):
 
     def test1(self):
-        h = "1 3 1 3 1 4 1 3 2 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5"
-        word = "abc"
+        testWeights = '1 3 1 3 1 4 1 3 2 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5'
+        h = map(int, testWeights.strip().split(' '))
+        word = 'abc'
         self.assertEqual(DesignerPDFViewer.pdfSelection(h, word), 9)
 
 if __name__ == "__main__":
