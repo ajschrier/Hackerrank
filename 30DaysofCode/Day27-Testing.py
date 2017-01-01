561c83af38b9d3d1e35e4db5a10920d5b248bc0d
AJ Schrier <ajschrier@gmail.com>
2016-12-31 21:38:38 -0800

    Completed Day27-Testing

---
+++ 30DaysofCode/Day27-Testing.py
@@ -0,0 +1,11 @@
+print('5')
+print('7 6')  # yes
+print('-3 -1 -6 5 4 -2 0')
+print('6 5')  # no
+print('-3 -1 -5 -4 2 0')
+print('5 2')  # yes
+print('3 1 6 5 0')
+print('9 7')  # no
+print('-3 -1 -6 -5 -4 2 0 -2 0')
+print('10 8')  # yes
+print('-3 -1 -6 5 0 -3 -1 6 5 0')
