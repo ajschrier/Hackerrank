2a772029f36372698ca2c6d5dab4aac1e3e03d6d
AJ Schrier <ajschrier@gmail.com>
2017-01-08 12:51:10 -0800

    Flake8 edits

--- Python/leftrotation.py
+++ Python/leftrotation.py
@@ -1,15 +1,9 @@
-from collections import deque
-
 def array_left_rotation(a, n, k):
-	for i in xrange(k):
-		a.append(a.pop(0))
-	return a
+    for i in xrange(k):
+        a.append(a.pop(0))
+    return a
 
 n, k = map(int, raw_input().strip().split(' '))
 a = map(int, raw_input().strip().split(' '))
-answer = array_left_rotation(a, n, k);
-print ' '.join(map(str,answer))
-
-
-
-
+answer = array_left_rotation(a, n, k)
+print ' '.join(map(str, answer))
